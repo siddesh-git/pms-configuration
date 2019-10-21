940c9dafb8ba5e8c263f89f8cd3e25b1cab13166
s0s01ts <siddesh.shivakumar@walmartlabs.com>
2019-10-22 00:25:53 +0530

    first commit

--- src/main/java/com/pawebrez/P09ConfigurationApplication.java
+++ src/main/java/com/pawebrez/P09ConfigurationApplication.java
@@ -2,13 +2,20 @@ package com.pawebrez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class P09ConfigurationApplication {
+public class P09ConfigurationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(com.pawebrez.P09ConfigurationApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(P09ConfigurationApplication.class);
 	}
 }
